a592293c38db221100e6b7725b516b89fe394caf
raja <rajamr9187@gmail.com>
2022-12-25 00:20:41 +0400

    day1

---
+++ App.js
@@ -0,0 +1,35 @@
+const heading4 = React.createElement(
+  "h2",
+  {
+    id: "welcome1",
+  },
+  "Namaste React Js-React DOM"
+);
+
+console.log(heading4);
+
+const heading3 = React.createElement(
+  "h1",
+  {
+    id: "welcome",
+  },
+  "Namaste React Js-React DOM"
+);
+
+console.log(heading3);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading4, heading3]
+);
+
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+
+console.log(root1);
+
+// passing a Element inside react root
+
+root1.render(container);
---
+++ App1.js
@@ -0,0 +1,13 @@
+const heading1 = document.createElement("h1");
+
+      console.log(heading1);
+
+      heading1.innerHTML = "Namaste React Js-javascript DOM";
+
+      console.log(heading1);
+
+      const root = document.getElementById("root");
+
+      console.log(root);
+
+      root.appendChild(heading1);
